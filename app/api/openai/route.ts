b1bfb9c2f6bc0c6cb74ce80cd6784324ef4f70c2
Rayjay-8 <27372821+Rayjay-8@users.noreply.github.com>
2023-05-13 23:24:49 -0300

    test router

--- app/api/openai/route.ts
+++ app/api/openai/route.ts
@@ -1,11 +1,14 @@
 import { NextResponse } from 'next/server'
-import { redirect } from 'next/navigation';
-
+import  App, {OpenAiWrapper}  from './WrapperAPI'
 
 
 export const GET = async (req: Request) => {  
+  // const ints = await App()
+  console.log(OpenAiWrapper.listKeys())
+
   return NextResponse.json({
     in: new Date(), 
-    sucesso: true
+    sucesso: true,
+    OpenAiWrapper:OpenAiWrapper.listKeys()
   })
 }
